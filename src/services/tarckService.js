e78b007e57951a7f735d575e0776f71c88c4f8d2
RubenPari <rubenpari.developer@gmail.com>
2023-08-18 23:18:44 +0200

    added logging in tarckService.js

--- src/services/tarckService.js
+++ src/services/tarckService.js
@@ -4,73 +4,85 @@ import { BASE_URL } from "@env";
 
 const BASE_URL_TRACK = `${BASE_URL}/track`;
 
-const getSummary = async () => {
+const getSummaryTracks = async () => {
   const response = await axios.get(`${BASE_URL_TRACK}/summary`);
 
   if (response.status !== 200) {
+    console.log("error to fetch summary tracks" + response.data);
     return false;
   }
 
+  console.log("success to fetch summary tracks");
   return response.data;
 };
 
-const deleteByArtist = async (artistId) => {
+const deleteTracksByArtist = async (artistId) => {
   const response = await axios.delete(
     `${BASE_URL_TRACK}/by-artist/${artistId}`
   );
 
   if (response.status !== 200) {
+    console.log("error to delete tracks by artist");
     return false;
   }
 
+    console.log("success to delete tracks by artist");
   return response.data;
 };
 
-const deleteByGenre = async (genreName) => {
+const deleteTracksByGenre = async (genreName) => {
   const response = await axios.delete(
     `${BASE_URL_TRACK}/by-genre?name=${genreName}`
   );
 
   if (response.status !== 200) {
+    console.log("error to delete tracks by genre");
     return false;
   }
 
+  console.log("success to delete tracks by genre");
   return response.data;
 };
 
-const deleteByRange = async (min, max) => {
+const deleteTracksByRange = async (min, max) => {
   var requestUrl;
 
   // Validate min and max
   if (min > max || min < 0 || max < 0 || min === max) {
+    console.log("error to delete tracks by range");
     return false;
   }
 
   if (min && !max) {
+    console.log("error to delete tracks by range");
     requestUrl = `${BASE_URL_TRACK}/by-range?min=${min}`;
   }
 
   if (!min && max) {
+    console.log("error to delete tracks by range");
     requestUrl = `${BASE_URL_TRACK}/by-range?max=${max}`;
   }
 
   if (min && max) {
+    console.log("success to delete tracks by range");
     requestUrl = `${BASE_URL_TRACK}/by-range?min=${min}&max=${max}`;
   }
 
   const response = await axios.delete(requestUrl);
 
   if (response.status !== 200) {
+    console.log("error to delete tracks by range");
     return false;
   }
 
+    console.log("success to delete tracks by range");
   return response.data;
 };
 
 /**
  * NOTE: filePath NOT includes the name of the file
  */
-const deleteByFile = async (filePath) => {
+const deleteTracksByFile = async (filePath) => {
   let data = new FormData();
 
   data.append("file", {
@@ -91,16 +103,18 @@ const deleteByFile = async (filePath) => {
   const response = await axios(config);
 
   if (response.status !== 200) {
+    console.log("error to delete tracks by file");
     return false;
   }
 
+    console.log("success to delete tracks by file");
   return response.data;
 };
 
 export {
-  getSummary,
-  deleteByArtist,
-  deleteByGenre,
-  deleteByRange,
-  deleteByFile,
+    getSummaryTracks,
+    deleteTracksByArtist,
+    deleteTracksByGenre,
+    deleteTracksByRange,
+    deleteTracksByFile,
 };
